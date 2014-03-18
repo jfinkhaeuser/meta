f1c30c739eec867db93ff3f39243d99ae35895c6
Jens Finkhaeuser <jens@finkhaeuser.de>
2014-03-18 14:03:21 +0100

    Resolves #4
    New singleton implementation

---
+++ meta/singleton.h
@@ -0,0 +1,185 @@
+/**
+ * This file is part of meta.
+ *
+ * Author(s): Jens Finkhaeuser <jens@unwesen.co.uk>
+ *
+ * Copyright (c) 2014 Unwesen Ltd.
+ *
+ * This software is licensed under the terms of the GNU GPLv3 for personal,
+ * educational and non-profit use. For all other uses, alternative license
+ * options are available. Please contact the copyright holder for additional
+ * information, stating your intended usage.
+ *
+ * You can find the full text of the GPLv3 in the COPYING file in this code
+ * distribution.
+ *
+ * This software is distributed on an "AS IS" BASIS, WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.
+ **/
+#ifndef META_SINGLETON_H
+#define META_SINGLETON_H
+
+#ifndef __cplusplus
+#error You are trying to include a C++ only header file
+#endif
+
+#include <meta/meta-config.h>
+
+#include <utility>
+
+#include <meta/noncopyable.h>
+
+namespace meta {
+
+namespace detail {
+/**
+ * The default lock policy for the singleton does nothing. A lock policy you
+ * provide needs to:
+ *  - Be default-constructible (and therefore destructible)
+ *  - provide a lock() function
+ *  - provide an unlock() function
+ *  - provide a memory_barrier() function.
+ *
+ * The last requirement *may* be dropped on C++11 systems (assuming the C++11
+ * memory model is sufficiently thread-aware).
+ **/
+struct default_lock_policy
+{
+  inline void lock() {}
+  inline void unlock() {}
+  inline void memory_barrier() {}
+};
+
+
+/**
+ * The default destruction policy is to register the passed callback via the
+ * standard atexit() function. For testing, or for ordered destruction of
+ * singletons, it may be worthwhile implementing your own.
+ **/
+struct default_destruction_policy
+{
+  static inline void register_destruction_callback(void (*func)(void))
+  {
+    atexit(func);
+  }
+};
+
+
+/**
+ * Helper scope guard using lock policies.
+ **/
+template <typename T>
+struct scope_guard
+{
+  T & policy;
+
+  scope_guard(T & _policy)
+    : policy(_policy)
+  {
+    policy.lock();
+  }
+
+  ~scope_guard()
+  {
+    policy.unlock();
+  }
+};
+
+} // namespace detail
+
+/**
+ * Thread-safe singleton implementation, provided the provided thread policy
+ * works as intended.
+ *
+ * Us
+ **/
+template <
+  typename T,
+  typename lock_policyT = detail::default_lock_policy,
+  typename destruction_policyT = detail::default_destruction_policy
+>
+class singleton
+{
+public:
+  virtual ~singleton()
+  {
+    // Signal that this object is dead, but don't destroy things yet!
+    sm_instance_pointer = nullptr;
+    sm_destroyed = true;
+  }
+
+
+
+  template <typename... Args>
+  static inline T const & const_instance(Args && ... args)
+  {
+    return *const_cast<T*>(get_instance(std::forward<Args>(args)...));
+  }
+
+
+
+  template <typename... Args>
+  static inline T & mutable_instance(Args && ... args)
+  {
+    return *const_cast<T*>(get_instance(std::forward<Args>(args)...));
+  }
+
+
+private:
+  template <typename... Args>
+  static inline volatile T * get_instance(Args && ... args)
+  {
+    // Barrier before reading sm_instance_pointer
+    sm_lock_policy.memory_barrier();
+    if (!sm_instance_pointer) {
+
+      // Only create lock if there does not appear to be an instance
+      detail::scope_guard<lock_policyT> guard(sm_lock_policy);
+
+      // Barrier before reading sm_instance_pointer
+      sm_lock_policy.memory_barrier();
+      if (!sm_instance_pointer) {
+        volatile T * tmp = new T(std::forward<Args>(args)...);
+        sm_instance_pointer = tmp;
+
+        // Register function for destroying the singleton
+        destruction_policyT::register_destruction_callback(destroy_instance);
+      }
+    }
+    return sm_instance_pointer;
+  }
+
+
+  static void destroy_instance()
+  {
+    detail::scope_guard<lock_policyT> guard(sm_lock_policy);
+    sm_lock_policy.memory_barrier();
+    sm_instance_pointer->~T();
+  }
+
+
+  // Need to instanciate lock policy in case it has state (such as a mutex)
+  static lock_policyT   sm_lock_policy;
+  static volatile bool  sm_destroyed;
+  static volatile T *   sm_instance_pointer;
+};
+
+
+template <typename T, typename lock_policyT, typename destruction_policyT>
+lock_policyT
+singleton<T, lock_policyT, destruction_policyT>::sm_lock_policy = lock_policyT();
+
+template <typename T, typename lock_policyT, typename destruction_policyT>
+volatile bool
+singleton<T, lock_policyT, destruction_policyT>::sm_destroyed = false;
+
+
+template <typename T, typename lock_policyT, typename destruction_policyT>
+volatile T *
+singleton<T, lock_policyT, destruction_policyT>::sm_instance_pointer = nullptr;
+
+
+} // namespace meta
+
+#endif // guard
---
+++ test/test_singleton.cpp
@@ -0,0 +1,172 @@
+/**
+ * This file is part of meta.
+ *
+ * Author(s): Jens Finkhaeuser <jens@unwesen.co.uk>
+ *
+ * Copyright (c) 2014 Unwesen Ltd.
+ *
+ * This software is licensed under the terms of the GNU GPLv3 for personal,
+ * educational and non-profit use. For all other uses, alternative license
+ * options are available. Please contact the copyright holder for additional
+ * information, stating your intended usage.
+ *
+ * You can find the full text of the GPLv3 in the COPYING file in this code
+ * distribution.
+ *
+ * This software is distributed on an "AS IS" BASIS, WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.
+ **/
+
+#include <cppunit/extensions/HelperMacros.h>
+
+#include <meta/meta-config.h>
+
+#include <meta/singleton.h>
+
+namespace {
+
+// Destruction policy used to manually destroy instances
+struct manual_destruction_policy
+{
+  typedef void (*func_t)(void);
+
+  static func_t func;
+
+  static void register_destruction_callback(func_t _func)
+  {
+    func = _func;
+  }
+
+
+  static void destroy_now()
+  {
+    func();
+  }
+};
+
+manual_destruction_policy::func_t
+manual_destruction_policy::func = nullptr;
+
+
+
+// Singleton for the tests. We'll template it, because that allows us to
+// re-use it in several tests.
+template <
+  typename T,
+  typename policyT = meta::detail::default_destruction_policy
+>
+struct MySingleton
+  : public meta::singleton<
+      MySingleton<T, policyT>,
+      meta::detail::default_lock_policy,
+      policyT
+    >
+{
+  T member;
+
+  static bool destroyed;
+
+  MySingleton(T const & _member = 42)
+    : meta::singleton<
+        MySingleton<T, policyT>,
+        meta::detail::default_lock_policy,
+        policyT
+      >()
+    , member(_member)
+  {
+    destroyed = false;
+  }
+
+  ~MySingleton()
+  {
+    destroyed = true;
+  }
+};
+
+
+template <typename T, typename policyT>
+bool
+MySingleton<T, policyT>::destroyed = false;
+
+
+} // anonymous namespace
+
+class SingletonTest
+  : public CppUnit::TestFixture
+{
+public:
+  CPPUNIT_TEST_SUITE(SingletonTest);
+
+    CPPUNIT_TEST(testSingletonDefaultCtor);
+    CPPUNIT_TEST(testSingletonCtorParameters);
+    CPPUNIT_TEST(testDestruction);
+
+  CPPUNIT_TEST_SUITE_END();
+private:
+
+  void testSingletonDefaultCtor()
+  {
+    CPPUNIT_ASSERT_EQUAL(false, MySingleton<int>::destroyed);
+
+    MySingleton<int> & s = MySingleton<int>::mutable_instance();
+    CPPUNIT_ASSERT_EQUAL(int(42), s.member);
+
+    s.member = 666;
+    MySingleton<int> const & s2 = MySingleton<int>::const_instance();
+    CPPUNIT_ASSERT_EQUAL(int(666), s2.member);
+    CPPUNIT_ASSERT_EQUAL(s.member, s2.member);
+    CPPUNIT_ASSERT_EQUAL(&s.member, const_cast<int*>(&s2.member));
+
+    // Lives until end of program
+    CPPUNIT_ASSERT_EQUAL(false, MySingleton<int>::destroyed);
+  }
+
+
+
+  void testSingletonCtorParameters()
+  {
+    CPPUNIT_ASSERT_EQUAL(false, MySingleton<long>::destroyed);
+
+    MySingleton<long> & s = MySingleton<long>::mutable_instance(1234);
+    CPPUNIT_ASSERT_EQUAL(long(1234), s.member);
+
+    s.member = 666;
+    // Even though we pass parameters to the second instance, they're ignored
+    // because the object already exists.
+    MySingleton<long> const & s2 = MySingleton<long>::const_instance(1234);
+    CPPUNIT_ASSERT_EQUAL(long(666), s2.member);
+    CPPUNIT_ASSERT_EQUAL(s.member, s2.member);
+    CPPUNIT_ASSERT_EQUAL(&s.member, const_cast<long*>(&s2.member));
+
+    // Lives until end of program
+    CPPUNIT_ASSERT_EQUAL(false, MySingleton<long>::destroyed);
+  }
+
+
+
+  void testDestruction()
+  {
+    CPPUNIT_ASSERT_EQUAL(false, (MySingleton<float, manual_destruction_policy>::destroyed));
+
+    MySingleton<float, manual_destruction_policy> const & s =
+      MySingleton<float, manual_destruction_policy>::const_instance(3.14);
+    CPPUNIT_ASSERT_EQUAL(float(3.14), s.member);
+
+    CPPUNIT_ASSERT_EQUAL(false, (MySingleton<float, manual_destruction_policy>::destroyed));
+    manual_destruction_policy::destroy_now();
+    CPPUNIT_ASSERT_EQUAL(true, (MySingleton<float, manual_destruction_policy>::destroyed));
+
+    // Since the instance is detroyed, we can now re-create it, I hope!
+    MySingleton<float, manual_destruction_policy> const & s2 =
+      MySingleton<float, manual_destruction_policy>::const_instance(2.78);
+    CPPUNIT_ASSERT_EQUAL(false, (MySingleton<float, manual_destruction_policy>::destroyed));
+    CPPUNIT_ASSERT_EQUAL(float(2.78), s2.member);
+
+    manual_destruction_policy::destroy_now();
+    CPPUNIT_ASSERT_EQUAL(true, (MySingleton<float, manual_destruction_policy>::destroyed));
+  }
+};
+
+
+CPPUNIT_TEST_SUITE_REGISTRATION(SingletonTest);
