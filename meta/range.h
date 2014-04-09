04556b622611bc288f416f4e0fafc64835694a6b
Jens Finkhaeuser <jens@finkhaeuser.de>
2014-04-09 15:08:02 +0200

    Add minimal range implementation for some easier C++0x usage.

---
+++ meta/range.h
@@ -0,0 +1,82 @@
+/**
+ * This file is part of meta.
+ *
+ * Author(s): Jens Finkhaeuser <jens@unwesen.co.uk>
+ *
+ * Copyright (c) 2014 Unwesen Ltd.
+ *
+ * This software is licensed under the terms of the GNU GPLv3 for personal,
+ * educational and non-profit use. For all other uses, alternative license
+ * options are available. Please contact the copyright holder for additional
+ * information, stating your intended usage.
+ *
+ * You can find the full text of the GPLv3 in the COPYING file in this code
+ * distribution.
+ *
+ * This software is distributed on an "AS IS" BASIS, WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.
+ **/
+#ifndef META_RANGE_H
+#define META_RANGE_H
+
+#ifndef __cplusplus
+#error You are trying to include a C++ only header file
+#endif
+
+#include <meta/meta.h>
+
+#include <utility>
+
+namespace meta {
+
+/**
+ * This class provides a *simple* way for using a pair of iterators (forming
+ * a range) instead of a container in C++0x's new for loops. For a more complete
+ * imlementation of ranges, see boost::range or
+ * https://code.google.com/p/cxx1y-range/
+ *
+ * Use make_range for ease of use; make_range can also convert from a std::pair,
+ * such as returned by multimap::equal_range(), etc.
+ **/
+template <typename iterT>
+struct range
+{
+  constexpr range(iterT _begin, iterT _end)
+    : m_begin(_begin)
+    , m_end(_end)
+  {
+  }
+
+  constexpr iterT begin() const
+  {
+    return m_begin;
+  }
+
+  constexpr iterT end() const
+  {
+    return m_end;
+  }
+
+  iterT m_begin;
+  iterT m_end;
+};
+
+
+template <typename iterT>
+range<iterT>
+make_range(iterT begin, iterT end)
+{
+  return range<iterT>(begin, end);
+}
+
+template <typename iterT>
+range<iterT>
+make_range(std::pair<iterT, iterT> pair)
+{
+  return range<iterT>(pair.first, pair.second);
+}
+
+} // namespace meta
+
+#endif // guard
---
+++ test/test_range.cpp
@@ -0,0 +1,91 @@
+/**
+ * This file is part of meta.
+ *
+ * Author(s): Jens Finkhaeuser <jens@unwesen.co.uk>
+ *
+ * Copyright (c) 2009-2012 Jens Finkhaeuser.
+ * Copyright (c) 2013 Unwesen Ltd.
+ *
+ * This software is licensed under the terms of the GNU GPLv3 for personal,
+ * educational and non-profit use. For all other uses, alternative license
+ * options are available. Please contact the copyright holder for additional
+ * information, stating your intended usage.
+ *
+ * You can find the full text of the GPLv3 in the COPYING file in this code
+ * distribution.
+ *
+ * This software is distributed on an "AS IS" BASIS, WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.
+ **/
+
+#include <cppunit/extensions/HelperMacros.h>
+
+#include <meta/range.h>
+
+#include <vector>
+#include <map>
+
+class RangeTest
+    : public CppUnit::TestFixture
+{
+public:
+    CPPUNIT_TEST_SUITE(RangeTest);
+
+      CPPUNIT_TEST(testPointerRange);
+      CPPUNIT_TEST(testVectorRange);
+      CPPUNIT_TEST(testMultiMapRange);
+
+    CPPUNIT_TEST_SUITE_END();
+
+private:
+
+    void testPointerRange()
+    {
+      int container[] = {0, 1, 2, 3, 4, 5}; // 6 elements
+
+      auto r = meta::make_range(container, container + 6);
+      int c = 0;
+      for (auto x : r) {
+        CPPUNIT_ASSERT_EQUAL(c, x);
+        ++c;
+      }
+      CPPUNIT_ASSERT_EQUAL(int(6), c);
+    }
+
+
+
+    void testVectorRange()
+    {
+      std::vector<int> container = {0, 1, 2, 3, 4, 5}; // 6 elements
+
+      auto r = meta::make_range(container.begin(), container.end());
+      int c = 0;
+      for (auto x : r) {
+        CPPUNIT_ASSERT_EQUAL(c, x);
+        ++c;
+      }
+      CPPUNIT_ASSERT_EQUAL(int(6), c);
+    }
+
+
+
+    void testMultiMapRange()
+    {
+      std::multimap<int, int> container;
+      container.insert(std::pair<int, int>(42, 0));
+      container.insert(std::pair<int, int>(42, 1));
+      container.insert(std::pair<int, int>(666, 123));
+
+      auto r = meta::make_range(container.equal_range(42));
+      int c = 0;
+      for (auto x : r) {
+        CPPUNIT_ASSERT_EQUAL(c, x.second);
+        ++c;
+      }
+      CPPUNIT_ASSERT_EQUAL(int(2), c);
+    }
+};
+
+
+CPPUNIT_TEST_SUITE_REGISTRATION(RangeTest);
