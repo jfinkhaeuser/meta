04ff4abc698e631f7efe6ca56e99249bfbbca55d
Jens Finkhaeuser <jens@finkhaeuser.de>
2014-08-04 11:28:20 +0200

    Ugh, dealing with homebrew sucks.

--- travis/install.sh
+++ travis/install.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
 
+function brew_install {
+  pkg="$1"
+  if brew list | grep -q "$pkg" ; then
+    # Installed
+    if brew outdated | grep -q "$pkg" ; then
+      # Outdated
+      brew upgrade "$@"
+    else
+      # Up-to-date
+      true
+    fi
+  else
+    # Not installed
+    brew install "$@"
+  fi
+}
+
 case "${TRAVIS_OS_NAME}" in
   linux)
     sudo apt-get install libcppunit-dev clang cmake
     ;;
   osx)
-    brew install cppunit
+    brew_install cppunit
+    brew_install cmake
 #    brew install llvm --with-asan --with-clang --HEAD
-    brew install cmake
     ;;
   *)
     echo "Unsupported os '${TRAVIS_OS_NAME}', aborting."
