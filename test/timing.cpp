8a619bab9c01f2fbebbcc9f14b879f6fe01076f6
Jens Finkhaeuser <jens@finkhaeuser.de>
2015-11-26 14:13:07 +0100

    Add simple getrusage based timer to replace boost's. This is not (yet) portable.

---
+++ test/timing.cpp
@@ -0,0 +1,77 @@
+/**
+ * This file is part of meta.
+ *
+ * Author(s): Jens Finkhaeuser <jens@finkhaeuser.de>
+ *
+ * Copyright (c) 2014 Unwesen Ltd.
+ * Copyright (c) 2015 Jens Finkhaeuser.
+ *
+ * This software is licensed under the terms of the GNU GPLv3 for personal,
+ * educational and non-profit use. For all other uses, alternative license
+ * options are available. Please contact the copyright holder for additional
+ * information, stating your intended usage.
+ *
+ * You can find the full text of the GPLv3 in the COPYING file in this code
+ * distribution.
+ *
+ * This software is distributed on an "AS IS" BASIS, WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.
+ **/
+
+#include <meta/meta-config.h>
+
+#include <test/timing.h>
+
+#include <iostream>
+
+#include <sys/time.h>
+#include <sys/resource.h>
+
+#include <errno.h>
+#include <string.h>
+
+
+namespace test {
+namespace timing {
+
+timer::timer()
+  : m_last(get_current())
+{
+}
+
+
+
+timer::~timer()
+{
+}
+
+
+
+int64_t
+timer::duration()
+{
+  int64_t cur = get_current();
+  int64_t result = cur - m_last;
+  m_last = cur;
+  return result;
+}
+
+
+int64_t
+timer::get_current() const
+{
+  struct ::rusage usage;
+  int err = ::getrusage(RUSAGE_SELF, &usage);
+  if (err < 0) {
+    std::cerr << "Failed to get rusage: " << ::strerror(errno) << std::endl;
+    return -1;
+  }
+
+  int64_t user = (static_cast<int64_t>(usage.ru_utime.tv_sec) * 1000000L)
+    + static_cast<int64_t>(usage.ru_utime.tv_usec);
+
+  return user;
+}
+
+}} // namespace test::timing
---
+++ test/timing.h
@@ -0,0 +1,56 @@
+/**
+ * This file is part of meta.
+ *
+ * Author(s): Jens Finkhaeuser <jens@finkhaeuser.de>
+ *
+ * Copyright (c) 2014 Unwesen Ltd.
+ * Copyright (c) 2015 Jens Finkhaeuser.
+ *
+ * This software is licensed under the terms of the GNU GPLv3 for personal,
+ * educational and non-profit use. For all other uses, alternative license
+ * options are available. Please contact the copyright holder for additional
+ * information, stating your intended usage.
+ *
+ * You can find the full text of the GPLv3 in the COPYING file in this code
+ * distribution.
+ *
+ * This software is distributed on an "AS IS" BASIS, WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.
+ **/
+
+#ifndef TEST_TIMING_H
+#define TEST_TIMING_H
+
+#ifndef __cplusplus
+#error You are trying to include a C++ only header file
+#endif
+
+#include <meta/meta.h>
+#include <meta/inttypes.h>
+
+namespace test {
+namespace timing {
+
+/**
+ * Measuring the own process' user time. System time might be a nice thing
+ * to add.
+ **/
+class timer
+{
+public:
+  timer();
+  ~timer();
+
+  int64_t duration();
+
+private:
+  int64_t get_current() const;
+
+  int64_t m_last;
+};
+
+}} // namespace test::timing
+
+
+#endif // guard
