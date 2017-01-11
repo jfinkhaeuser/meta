0dfcd5d65f1a88fb4c3c7f4b8589c29a3c5cbfad
Jens Finkhaeuser <jens@finkhaeuser.de>
2017-01-11 08:43:21 +0000

    Follow renaming of chroot.sh to run.sh

--- travis/run.sh
+++ travis/run.sh
@@ -47,7 +47,7 @@ function setup_arm_chroot {
     sudo touch ${CHROOT_DIR}/.chroot_is_done
 
     # Call ourselves again which will cause tests to run
-    sudo chroot ${CHROOT_DIR} bash -c "cd ${TRAVIS_BUILD_DIR} && ./travis/chroot.sh"
+    sudo chroot ${CHROOT_DIR} bash -c "cd ${TRAVIS_BUILD_DIR} && ./travis/run.sh"
 }
 
 if [ -e "/.chroot_is_done" ]; then
